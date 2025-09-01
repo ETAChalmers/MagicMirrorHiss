43683df66b3e6304f962bd08ed65827cbac50675
audiotrope <122730216+audiotrope@users.noreply.github.com>
2025-09-01 20:27:39 +0200

    Setup project, remove module in wrong dir

--- config/config.js
+++ config/config.js
@@ -39,72 +39,16 @@ let config = {
 
 	modules: [
 		{
-			module: "alert",
+		  module: 'MMM-Videoplayer',
+		  position: 'middle_center',
+		  config:	{
+			video: "/modules/MMM-Videoplayer/video/etamirror.mp4", // Can also be a URL to a mp4 file on the internet.
+			loop: true, // Repeat the video.
+			autoplay: true, // If set to true, sound (muted by default) has to be muted, otherwise the video will not auto play.
+			notification: "VIDEOPLAYER1", // Unique notification string for this player (to be able to play, pause, restart and next from another modules).
+		  			}
 		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
-		{
-			module: "clock",
-			position: "top_left"
-		},
-		{
-			module: "calendar",
-			header: "US Holidays",
-			position: "top_left",
-			config: {
-				calendars: [
-					{
-						fetchInterval: 7 * 24 * 60 * 60 * 1000,
-						symbol: "calendar-check",
-						url: "https://ics.calendarlabs.com/76/mm3137/US_Holidays.ics"
-					}
-				]
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
-		},
-		{
-			module: "weather",
-			position: "top_right",
-			config: {
-				weatherProvider: "openmeteo",
-				type: "current",
-				lat: 40.776676,
-				lon: -73.971321
-			}
-		},
-		{
-			module: "weather",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				weatherProvider: "openmeteo",
-				type: "forecast",
-				lat: 40.776676,
-				lon: -73.971321
-			}
-		},
-		{
-			module: "newsfeed",
-			position: "bottom_bar",
-			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true,
-				broadcastNewsFeeds: true,
-				broadcastNewsUpdates: true
-			}
-		},
-	]
+			]
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
